8e8e0ecd48bf518c113187f965c01aabcd6e2fc1
Enzo Feltrin <enzobrexo@gmail.com>
2025-06-16 15:49:42 -0300

    created the pokemon.service for api calling and new functions

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Pokemon, PokemonListResponse, PokemonType } from '../types/PokemonType';
 
 @Injectable({
@@ -12,14 +13,24 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getPokemonList(limit = 20, offset = 0): Observable<PokemonListResponse> {
-    return this.http.get<PokemonListResponse>(`${this.apiUrl}/pokemon?limit=${limit}&offset=${offset}`);
+    return this.http.get<PokemonListResponse>(`${this.apiUrl}/pokemon?limit=${limit}&offset=${offset}`)
+      .pipe(
+        catchError(() => throwError(() => new Error('Failed to fetch Pokémon list')))
+      );
   }
 
   getPokemonByName(name: string): Observable<Pokemon> {
-    return this.http.get<Pokemon>(`${this.apiUrl}/pokemon/${name}`);
+    return this.http.get<Pokemon>(`${this.apiUrl}/pokemon/${name}`)
+      .pipe(
+        catchError(() => throwError(() => new Error('Failed to fetch Pokémon')))
+      );
   }
 
   getPokemonTypeByName(typeName: string): Observable<PokemonType> {
-    return this.http.get<PokemonType>(`${this.apiUrl}/type/${typeName}`);
+    return this.http.get<PokemonType>(`${this.apiUrl}/type/${typeName}`)
+      .pipe(
+        catchError(() => throwError(() => new Error('Failed to fetch Pokémon type')))
+      );
   }
+
 }
